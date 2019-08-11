cdf60f306a7a8d57b5a2158c4ab9f06dfd345741
nothinghappens <van20131002@gmail.com>
2019-08-11 12:12:30 +0800

    selectBox可刪除參數

--- src/plugins/url.js
+++ src/plugins/url.js
@@ -1,19 +1,21 @@
 let realurl = {
-  "selectParam": "/send/param",
-  "diagnoseImage": "/predict/send/new",
-  "getColor": "/send/img/back",
-  "sendConfirm": "/send/dbset",
-  "upload": "/add/data/training",
-  "retrain": "/add/data/retrain"
+  selectParam: "/send/param",
+  deleteParam: "/send/param",
+  diagnoseImage: "/predict/send/new",
+  getColor: "/send/img/back",
+  sendConfirm: "/send/dbset",
+  upload: "/add/data/training",
+  retrain: "/add/data/retrain"
 };
 
 let fakeurl = {
-  "selectParam": "http://localhost/v6/public/options.php",
-  "diagnoseImage": "http://localhost/v6/public/input.php",
-  "getColor": "http://localhost/v6/public/modify.php",
-  "sendConfirm": "http://localhost/v6/public/confirm.php",
-  "upload": "http://localhost/v6/public/input.php",
-  "retrain": "http://localhost/v6/public/input.php"
+  selectParam: "http://localhost/v6/public/options.php",
+  deleteParam: "http://localhost/v6/public/options.php",
+  diagnoseImage: "http://localhost/v6/public/input.php",
+  getColor: "http://localhost/v6/public/modify.php",
+  sendConfirm: "http://localhost/v6/public/confirm.php",
+  upload: "http://localhost/v6/public/input.php",
+  retrain: "http://localhost/v6/public/input.php"
 };
 
 export default (process.env.NODE_ENV == "development" ? fakeurl : realurl);
--- src/views/components/selectBox.vue
+++ src/views/components/selectBox.vue
@@ -7,6 +7,7 @@
       </v-btn-toggle>
       <v-card-actions>
         <v-btn color="secondary" block dark @click="returnOptions">Confirm</v-btn>
+        <v-btn color="secondary" block dark class="red" @click="deleteOption">DELETE</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -16,43 +17,56 @@
 export default {
   props: ["options"],
   data: () => ({
-    open:false,
+    open: false,
     index: null,
     newOptions: null
   }),
   methods: {
     returnOptions() {
-      this.$emit("returnSelect",this.newOptions[this.index])
-      this.open = false
+      this.$emit("returnSelect", this.newOptions[this.index]);
+      this.open = false;
     },
+    deleteOption() {
+      this.api
+        .delete(this.url.deleteParam + "?param=" + this.newOptions[this.index])
+        .then(() => {
+          this.newOptions.splice(this.index, 1);
+          this.open = false;
+        })
+        .catch(err =>
+          this.swal({
+            title: err
+          })
+        );
+    }
   },
-  watch:{
+  watch: {
     options() {
-      this.newOptions = this.options
-      this.open = this.options ? true : false
+      this.newOptions = this.options;
+      this.open = this.options ? true : false;
     }
   }
 };
 </script>
 
 <style>
-._optionBox{
-  display:block;
-  padding:10px 20px;
-  min-height:40vh;
+._optionBox {
+  display: block;
+  padding: 10px 20px;
+  min-height: 40vh;
   box-shadow: 0px !important;
 }
 ._option {
   width: calc(100% - 10px) !important;
   box-shadow: 0px !important;
 }
-._option.v-btn.v-btn--active{
-  border:1px solid blue;
+._option.v-btn.v-btn--active {
+  border: 1px solid blue;
 }
-._option.v-btn.v-btn--active::before{
-  background-color:lightblue;
+._option.v-btn.v-btn--active::before {
+  background-color: lightblue;
 }
-._option .v-btn__content{
-  justify-content: right
+._option .v-btn__content {
+  justify-content: right;
 }
 </style>
